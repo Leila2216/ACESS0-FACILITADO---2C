057de7a86ed6536fb6c83ed7f508e5dc3e9b2a06
Leila2216 <00001111399359sp@al.educacao.sp.gov.br>
2025-09-08 07:30:20 -0300

    g

--- script.js
+++ script.js
@@ -31,5 +31,8 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.classList.toggle('alto-contraste')
     })
 
+const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+
 
 })
